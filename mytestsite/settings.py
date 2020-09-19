d29f72af61bb0ec537532e1e50d8a5ec53e0da8b
Alex <korchakoleksiy@gmail.com>
2020-09-19 13:59:51 +0300

    False posgit4

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mytestsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogDB',
+        'NAME': 'blogdb',
         'USER': 'blog_admin',
         'PASSWORD': 'testing123',
         'HOST': 'localhost',
