c24eb8e0646034a9a56fe221afdd7607869b9192
Alex <korchakoleksiy@gmail.com>
2020-09-19 13:18:48 +0300

    Posgret static roots changes

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'mytestsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogedb',
+        'USER': 'blog_admin',
+        'PASSWORD': 'testing123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
